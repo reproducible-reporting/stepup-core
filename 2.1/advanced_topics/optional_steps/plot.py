84ad1a32fe4b1a7f6ac37f1c59550d76759ec0d6
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2025-02-12 09:38:53 +0100

    Deployed f738f2a4 to 2.1 with MkDocs 1.6.1 and mike 2.1.3

---
+++ 2.1/advanced_topics/optional_steps/plot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+from stepup.core.script import driver
+
+
+def info():
+    r = 3.2
+    return {
+        "inp": ["matplotlibrc", f"logmap_{r:5.3f}.txt"],
+        "out": "plot_logmap.png",
+    }
+
+
+def run(inp, out):
+    mpl.rc_file(inp[0])
+    seq = np.loadtxt(inp[1])
+    fig, ax = plt.subplots()
+    ax.plot(seq)
+    ax.set_xlabel("n")
+    ax.set_ylabel("x_n")
+    fig.savefig(out)
+
+
+if __name__ == "__main__":
+    driver()
